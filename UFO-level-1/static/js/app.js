0beff95fc8df8d510780e422c1c8403a9c3601ac
Selom <selomk@hotmail.com>
2021-04-16 16:36:29 -0500

    Folder update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -22,4 +22,36 @@ function tableDisplay(ufoEvents) {
       .selectAll("td").remove();
   };
   
+  // Show UFO Events
+  console.log(tableData);
+  tableDisplay(tableData);
   
+  // Filtering
+  var button = d3.select("#filter-btn");
+  
+  // Display
+  button.on("click", function(event) {
+    d3.event.preventDefault();
+    deleteTbody();
+    var dateInput = d3.select("#datetime").property("value");
+    
+    if (dateInput.trim() === "" ) {
+      var filteredData = tableData;
+    } else {  
+      var filteredData = tableData.filter(ufoEvents => 
+        ufoEvents.datetime === dateInput.trim());
+    };
+  
+    // If there is no record, display
+    if (filteredData.length == 0) {
+      d3.select("tbody")
+        .append("tr")
+        .append("td")
+          .attr("colspan", 7)
+          .html("<h4>No Records</h4>");
+    };
+  
+    // Show table records
+    console.log(filteredData);
+    tableDisplay(filteredData);
+  });
